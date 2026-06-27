4d5cdf61134e65c0b6519ae9a85c81d50531a61c
Noma Mgedezi <nomamgedezii@icloud.com>
2026-06-27 13:24:34 +0200

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,7 +1,5 @@
+-- Quality Checks
 /*
-===================================================================================
-Quality Checks
-===================================================================================
 Script Purpose:
     This script perfoms various quality checks for data consistency, accurancy, and
     standardization across the 'Silver' schema. It includes checks for:
@@ -14,12 +12,10 @@ Script Purpose:
 Usage Notes:
       - Run these checks after data loading silver layer.
       - Investigate and resolve any discrepancies found during checks.
-======================================================================================
 */
---====================================================================================
--- Checking 'silver.crm_cust_info'
---====================================================================================
--- Check For Nulls or Duplicates in Primary Key
+
+-- Checking silver.crm_cust_info
+-- Checking For Nulls or Duplicates in Primary Key
 -- Expectation: No Result
 SELECT 
 cst_id,
@@ -28,7 +24,7 @@ FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL
 
--- Check for unwanted Spaces
+-- Checking for unwanted Spaces
 -- Expectation: No Result
 SELECT cst_lastname
 FROM silver.crm_cust_info
@@ -40,10 +36,9 @@ FROM silver.crm_cust_info
 
 SELECT * FROM silver.crm_cust_info
 
---====================================================================================
--- Checking 'silver.crm_prd_info'
---====================================================================================
--- Check For Nulls or Duplicates in Primary Key
+	
+-- Checking silver.crm_prd_info
+-- Checking For Nulls or Duplicates in Primary Key
 -- Expectation: No Result
 SELECT 
 prd_id,
@@ -52,13 +47,13 @@ FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL
 
--- Check for unwanted Spaces
+-- Checking for unwanted Spaces
 -- Expectation: No Result
 SELECT prd_nm
 FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm)
 
--- Check for Nulls or Negative Numbers
+-- Checking for Nulls or Negative Numbers
 -- Expectation: No Results
 SELECT prd_cost
 FROM silver.crm_prd_info
@@ -68,7 +63,7 @@ WHERE prd_cost < 0 OR prd_cost IS NULL
 SELECT DISTINCT prd_line
 FROM silver.crm_prd_info
 
--- Check for Invalid Date Orders
+-- Checking for Invalid Date Orders
 SELECT *
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
@@ -88,12 +83,8 @@ FROM bronze.crm_prd_info
 WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
 
 
---====================================================================================
--- Checking 'silver.crm_sales_details'
---====================================================================================
-
--- Check for Invalid Dates
-
+-- Checking silver.crm_sales_details
+-- Checking for Invalid Dates
 SELECT 
 NULLIF(sls_order_dt,0) sls_order_dt
 FROM bronze.crm_sales_details
@@ -107,13 +98,13 @@ WHERE sls_due_dt < 0
 	OR sls_due_dt > 20500101
 	OR sls_due_dt < 19000101
 
--- Check for Invalid Date Orders
+-- Checking for Invalid Date Orders
 SELECT 
 *
 FROM  silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 
--- Check Data Consistency: Between Sales, Quantity, and Price
+-- Checking Data Consistency: Between Sales, Quantity, and Price
 -- >> Sales = Quantity * Price
 -- >> Values must not be NUll, zero, or negative.
 
@@ -150,7 +141,7 @@ FROM bronze.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
  OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL OR sls_price IS NULL
 
--- Check for Invalid Dates
+-- Checking for Invalid Dates
 SELECT 
 NULLIF(sls_due_dt, 0) sls_due_dt
 FROM bronze.crm_sales_details
@@ -159,13 +150,13 @@ WHERE sls_due_dt < 0
 	OR sls_due_dt > 20500101
 	OR sls_due_dt < 19000101
 
--- Check for Invalid Date Orders
+-- Checking for Invalid Date Orders
 SELECT 
 *
 FROM  silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 
--- Check Data Consistency: Between Sales, Quantity, and Price
+-- Checking Data Consistency: Between Sales, Quantity, and Price
 -- >> Sales = Quantity * Price
 -- >> Values must not be NUll, zero, or negative.
 
@@ -183,10 +174,7 @@ ORDER BY sls_sales, sls_quantity, sls_price
  SELECT * FROM silver.crm_sales_details
 
 
---====================================================================================
 -- Checking 'silver.erp_cust_az12'
---====================================================================================
-
 -- Identify Out-of-Range Dates
 SELECT DISTINCT
 bdate 
@@ -233,9 +221,7 @@ END AS gen
 FROM bronze.erp_cust_az12
 
   
---====================================================================================
 -- Checking 'silver.erp_loc_a101'
---====================================================================================
 SELECT 
 REPLACE(cid, '-', '' ) cid,
 cntry
@@ -269,9 +255,7 @@ ORDER BY cntry
 SELECT * FROM silver.erp_loc_a101
 
   
---====================================================================================
 -- Checking 'silver.erp_px_cat_g1v2'
---====================================================================================
 -- Check for unwanted spaces
 SELECT * FROM bronze.erp_px_cat_g1v2
 WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
