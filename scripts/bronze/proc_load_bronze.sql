8be50a811b41bb93bb9ca20f812fb2294ea81111
Noma Mgedezi <nomamgedezii@icloud.com>
2026-06-27 12:26:04 +0200

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -29,10 +29,8 @@ BEGIN
 
         -- bronze.crm_cust_info --
 		SET @start_time = GETDATE();
-		PRINT 'Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
 
-		PRINT 'Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_crm\cust_info.csv'
 		WITH  (
@@ -43,10 +41,8 @@ BEGIN
 		
 
         -- bronze.crm_prd_info --
-		PRINT 'Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 
-		PRINT 'Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_crm\prd_info.csv'
 		WITH (
@@ -58,10 +54,8 @@ BEGIN
 
 
         -- bronze.crm_sales_details --
-		PRINT 'Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
 
-		PRINT 'Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_crm\sales_details.csv'
 		WITH (
@@ -76,10 +70,8 @@ BEGIN
 		PRINT '---------------------------------------------------';
 
         -- bronze.erp_loc_a101 --
-		PRINT 'Truncating Table: bronze.erp_loc_a101';
 		TRUNCATE TABLE bronze.erp_loc_a101;
 
-		PRINT 'Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_erp\loc_a101.csv'
 		WITH (
@@ -89,10 +81,8 @@ BEGIN
 		);
 
         -- bronze.erp_cust_az12 --
-		PRINT 'Truncating Table: bronze.erp_cust_az12';
 		TRUNCATE TABLE bronze.erp_cust_az12;
 
-		PRINT 'Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_erp\cust_az12.csv'
 		WITH (
@@ -103,10 +93,8 @@ BEGIN
 
 
         -- bronze.erp_px_cat_g1v2 --
-		PRINT 'Truncating Table: bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
-		PRINT 'Inserting Data Into: bronze.erp_px_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
 		FROM 'C:\SQL\data_warehouse_project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
@@ -116,13 +104,13 @@ BEGIN
 		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST( DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
-		PRINT '>>---------------';
+		PRINT '>> --------------';
 
 		SET @batch_end_time = GETDATE();
 		PRINT'==============================================';
 		PRINT'Loading Bronze Layer is Completed';
 		PRINT '	- Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + 'seconds';  
-		PRINT'==============================================';
+		PRINT'>> ---------------';
 	
      END TRY
   BEGIN CATCH
