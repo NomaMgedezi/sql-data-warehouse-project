307101c3eb6ab6c94d5bc78a5fc0756928fd37fd
Noma Mgedezi <nomamgedezii@icloud.com>
2026-06-28 14:48:01 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,7 +1,5 @@
+-- Stored Procedure: Load Silver Layer (Bronze to Silver)
 /*
-=======================================================================================
-Stored Procedure: Load Silver Layer (Bronze to Silver)
-=======================================================================================
 Script Purpose:
     This stored procedure performs the ETL (Extract Teansform Load) to populate the 'silver' schema 
 	tables from the 'bronze' schema.
@@ -16,49 +14,49 @@ BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY 
 		SET @batch_start_time = GETDATE();
-		PRINT '===================================================';
-		PRINT 'Loading Silver Layer';
-		PRINT '===================================================';
 
 		PRINT '---------------------------------------------------';
-		PRINT 'Loading CRM Tables';
+		PRINT 'Loading Silver Layer';
 		PRINT '---------------------------------------------------';
 
 		-- Loading silver.crm_cust_info
 		SET @start_time = GETDATE();
-	PRINT '>> Truncating Table: silver.crm_cust_info';
+	PRINT 'Truncating Table: silver.crm_cust_info';
 	TRUNCATE TABLE silver.crm_cust_info;
-	PRINT '>> Inserting Data Into: silver.crm_cust_info';
-	INSERT INTO silver.crm_cust_info (
-  		cst_id,
-  		cst_key,
-  		cst_firstname,
-  		cst_lastname,
-  		cst_marital_status,
-  		cst_gndr,
-  		cst_create_date
-	)
-	SELECT 
-	cst_id,
-	cst_key,
-	TRIM(cst_firstname) AS cst_firstname,
-	TRIM(cst_lastname) AS cst_lastname,
-	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-		 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Maried'
-		 ELSE 'n/a'
-	END cst_marital_status, -- Normalize Marital Status values to readable format
+	PRINT 'Inserting Data Into: silver.crm_cust_info';
+	-- Transformation to cleanup the two columns (removing unwanted Spaces) to ensure Data Consistency
+INSERT INTO silver.crm_cust_info (
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date)
 
-	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-		 ELSE 'n/a'
-	END cst_gndr,  -- Normalize Gender values to readable format
-	cst_create_date
-	FROM(
-	SELECT 
-	*,
-	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last 
-	FROM bronze.crm_cust_info
-	) t WHERE flag_last = 1 -- Select the most recent record per customer
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Maried'
+	 ELSE 'n/a'
+END cst_marital_status, -- Normalize Marital Status values to readable format
+
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,  -- Normalize Gender values to readable format
+cst_create_date
+FROM(
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last 
+FROM bronze.crm_cust_info
+) t WHERE flag_last = 1 -- Select the most recent record per customer
+    AND cst_id IS NOT NULL;
+
 	SET @end_time = GETDATE();
 	PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + 'seconds';
 	PRINT '>> ---------------';
@@ -66,9 +64,9 @@ BEGIN
 
 	-- Loading silver.crm_prd_info
 	SET @start_time = GETDATE();
-	PRINT '>> Truncating Table: silver.crm_prd_info';
+	PRINT 'Truncating Table: silver.crm_prd_info';
 	TRUNCATE TABLE silver.crm_prd_info;
-	PRINT '>> Inserting Data Into: silver.crm_prd_info';
+	PRINT 'Inserting Data Into: silver.crm_prd_info';
 	INSERT INTO silver.crm_prd_info (
   		prd_id,
   		cat_id,
@@ -105,9 +103,9 @@ BEGIN
 
 	-- Loading silver.crm_sales_details
 	SET @start_time = GETDATE();
-	PRINT '>> Truncating Table: silver.crm_sales_details';
+	PRINT 'Truncating Table: silver.crm_sales_details';
 	TRUNCATE TABLE silver.crm_sales_details;
-	PRINT '>> Insert Data Into: silver.crm_sales_details';
+	PRINT 'Inserting Data Into: silver.crm_sales_details';
 	INSERT INTO silver.crm_sales_details (
   		sls_ord_num,
   		sls_prd_key,
@@ -149,9 +147,9 @@ BEGIN
 
 	-- Loading silver.erp_cust_az12
 	SET @start_time = GETDATE();
-	PRINT '>> Truncating Table: silver.erp_cust_az12';
+	PRINT 'Truncating Table: silver.erp_cust_az12';
 	TRUNCATE TABLE silver.erp_cust_az12;
-	PRINT '>> Insert Data Into: silver.erp_cust_az12';
+	PRINT 'Inserting Data Into: silver.erp_cust_az12';
 	INSERT INTO silver.erp_cust_az12(
   	cid,
   	bdate,
@@ -176,9 +174,9 @@ BEGIN
 
 	-- Loading silver.erp_loc_a101
 	SET @start_time = GETDATE();
-	PRINT'>> Truncating Table: silver.erp_loc_a101';
+	PRINT'Truncating Table: silver.erp_loc_a101';
 	TRUNCATE TABLE silver.erp_loc_a101;
-	PRINT'>> Insert Data Into: silver.erp_loc_a101';
+	PRINT'Inserting Data Into: silver.erp_loc_a101';
 	INSERT INTO silver.erp_loc_a101
 	(
   		cid,
@@ -199,9 +197,9 @@ BEGIN
 
 	-- Loading silver.erp_px_cat_g1v2
 	SET @start_time = GETDATE();
-	PRINT'>> Truncate Table: silver.erp_px_cat_g1v2';
+	PRINT'Truncate Table: silver.erp_px_cat_g1v2';
 	TRUNCATE TABLE silver.erp_px_cat_g1v2;
-	PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+	PRINT 'Inserting Data Into: silver.erp_px_cat_g1v2';
 	INSERT INTO silver.erp_px_cat_g1v2
 	(
   		id,
@@ -222,8 +220,8 @@ BEGIN
 	SET @batch_end_time = GETDATE();
 	PRINT '==================================';
 	PRINT 'Loading Silver Layer is Copleted';
-	PRINT '	- Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + 'seconds';
-	PRINT '===================================';
+	PRINT '	- Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + 'seconds';
+	PRINT '------------------';
 
 END TRY
 BEGIN CATCH 
