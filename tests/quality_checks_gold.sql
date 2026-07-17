e336ad38b3093aac520012d5b8f495cf5406815f
Noma Mgedezi <nomamgedezii@icloud.com>
2026-07-17 21:24:43 +0200

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -8,21 +8,21 @@ Script Purpose:
     - Validation of relationships in the data model for analytical purposes.
 */
 
--- Check for uniqueness of Product Key in gold.dim_products 
--- Expectation: No result
-SELECT
-    product_key, COUNT(*) AS duplicate_count
-FROM gold.dim_products
-GROUP BY product_key
-HAVING COUNT(*) > 1;
-
-
 -- Check for uniqueness of Customer Key in gold.dim_customers
 -- Expectation: No result
 SELECT 
     customer_key, COUNT(*) AS duplicate_count
 FROM gold.dim_customers
 GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+
+-- Check for uniqueness of Product Key in gold.dim_products
+-- Expectation: No result
+SELECT
+    product_key, COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
 HAVING COUNT(*) > 1;
 
 
@@ -35,4 +35,28 @@ ON c.customer_key = f.customer_key
 LEFT JOIN gold.dim_products p
 ON p.product_key = f.product_key
 WHERE p.product_key IS NULL
-    
+
+
+SELECT COUNT(*) AS missing_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p ON p.product_key = f.product_key
+WHERE p.product_key IS NULL;
+
+
+
+SELECT COUNT(*) AS missing_customers
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c ON c.customer_key = f.customer_key
+WHERE c.customer_key IS NULL;
+
+SELECT
+    (SELECT COUNT(*) FROM gold.fact_sales f
+     LEFT JOIN gold.dim_products p ON p.product_key = f.product_key
+     WHERE p.product_key IS NULL) AS missing_products,
+    (SELECT COUNT(*) FROM gold.fact_sales f
+     LEFT JOIN gold.dim_customers c ON c.customer_key = f.customer_key
+     WHERE c.customer_key IS NULL) AS missing_customers;
+
+
+
+
